3b53d4e1962ad481a6b4c1cff768a5a58fd8eafd
RicchiutiLucia <luciaricchiuti04@gmail.com>
2023-04-17 15:27:24 +0200

    aggiunto un po di stile, esercizio concluso

--- censura.php
+++ censura.php
@@ -15,17 +15,22 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
     <title>Censura la parola!</title>
 </head>
 <body>
-    <h2>Il testo inserito e la lunghezza del paragrafo:</h2>
-    <p><?php echo "$testo è di  $lunghezzaParagrafo caratteri" ?></p>
+    <div id="result">
+        <h2>Il testo inserito e la lunghezza del paragrafo:</h2>
+        <p><?php echo "$testo è di  $lunghezzaParagrafo caratteri" ?></p>
 
-    <h3>Parola che hai deciso di censurare:</h3>
-    <p><?php echo $censura; ?></p>
+        <h3>Parola che hai deciso di censurare:</h3>
+        <p><?php echo $censura; ?></p>
 
-    <h3>Testo censurato e nuova lunghezza del testo:</h3>
-    <p><?php echo "$testoCensurato è di $lunghezzaParagrafoCensurato caratteri" ?></p>
+        <h3>Testo censurato e nuova lunghezza del testo:</h3>
+        <p><?php echo "$testoCensurato è di $lunghezzaParagrafoCensurato caratteri" ?></p>
+
+    </div>
+    
     
     
 </body>
--- index.php
+++ index.php
@@ -4,15 +4,18 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
     <title>Censura la parola!</title>
 </head>
 <body>
     <form action="censura.php" method="GET">
 
-        <label for="testo">Inserisci un testo</label> <br />
+
+
+        <label for="testo">Inserisci un testo:</label>
         <input type="text" name="text" id="testo" placeholder="Testo a piacere" /> <br />
 
-        <label for="censura">Inserisci una parola da censurare</label> <br />
+        <label for="censura">Inserisci una parola da censurare:</label>
         <input type="text" name="censura" id="censura" placeholder="Parola da censurare" /> <br />
         <button type="submit">Controlla il testo</button>
 
